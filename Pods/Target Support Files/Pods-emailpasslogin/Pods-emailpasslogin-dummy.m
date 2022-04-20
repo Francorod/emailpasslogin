25478b0b8f1b21ed99e1f3ed4df1c4b0b87cd7e8
Franco Rod <francorodios@gmail.com>
2022-04-20 09:13:14 -0400

    Add elements

---
+++ Pods/Target Support Files/Pods-emailpasslogin/Pods-emailpasslogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_emailpasslogin : NSObject
+@end
+@implementation PodsDummy_Pods_emailpasslogin
+@end
--- emailpasslogin/ViewController.swift
+++ emailpasslogin/ViewController.swift
@@ -8,10 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Log In"
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        return label
+    }()
+    
+    private let emailField: UITextField = {
+        let emailField = UITextField()
+        emailField.textAlignment = .center
+        emailField.text = "Email Address"
+        emailField.font = .systemFont(ofSize: 24, weight: .semibold)
+        return emailField
+    }()
+    
+    private let passwordField: UITextField = {
+        let passField = UITextField()
+        passField.textAlignment = .center
+        passField.text = "Log In"
+        passField.font = .systemFont(ofSize: 24, weight: .semibold)
+        return passField
+    }()
+    
+    private let Button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Continue", for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(label)
+        view.addSubview(emailField)
+        view.addSubview(passwordField)
+        view.addSubview(Button)
     }
 
 
